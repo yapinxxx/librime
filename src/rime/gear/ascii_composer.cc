2e699812d490c1752d7149788dae233e7deaa702
niauah <50414882+niauah@users.noreply.github.com>
2020-12-04 14:15:55 +0800

    Update ascii_composer.cc

--- src/rime/gear/ascii_composer.cc
+++ src/rime/gear/ascii_composer.cc
@@ -64,8 +64,8 @@ static bool kiamtsa_kauki() {
   tm1.tm_hour = 23;
   tm1.tm_min = 59;
   tm1.tm_sec = 0;
-  tm1.tm_year = 2020-1900;
-  tm1.tm_mon = 12-1;
+  tm1.tm_year = 2021-1900;
+  tm1.tm_mon = 1-1;
   tm1.tm_mday = 31;
   time_t kauki = mktime(&tm1);
   if(chitma > kauki){
